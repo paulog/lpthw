190de0a32bcc3969093526dfba3dbc464c020a7d
Paulo Gonzaga <paulo.gonzaga@gmail.com>
2014-06-22 13:44:03 -0400

    Add raw_input

--- ex13.py
+++ ex13.py
@@ -3,7 +3,10 @@ from sys import argv
 script, first, second, third = argv
 
 print "The script is called:", script
-print "Your first variable is:", first
-print "Your second variable is:", second
-print "Your third variable is:", third
+
+name = raw_input("What is your name? ")
+
+print "%s, your first variable is:" % name, first
+print "%s, your second variable is:" % name, second
+print "%s, your third variable is:" % name, third
 
