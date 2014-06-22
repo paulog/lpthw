7bb3392898a443e6e7d60045e09b4f0e0450ef04
Paulo Gonzaga <paulo.gonzaga@gmail.com>
2014-06-22 14:02:01 -0400

    Add close

--- ex15.py
+++ ex15.py
@@ -1,15 +1,24 @@
+# Import modules
 from sys import argv
 
 script, filename = argv
 
+# Open the file from the command line argument
 txt = open(filename)
 
+# Print out the contents of the file
 print "Here's your file %r:" % filename
 print txt.read()
 
+txt.close()
+
+# As for a new file to read
 print "Type the filename again:"
 file_again = raw_input("> ")
 
+# Print out the contents of the raw_inputted file
 txt_again = open(file_again)
 
 print txt_again.read()
+
+txt_again.close()
